b42a36932b981884c544184a45cda053fbff5a17
Walter Oswaldo Gomez Mancipe <108770203+wogomezma@users.noreply.github.com>
2024-09-17 19:27:44 +0000

    Add dag with script

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 from sodapy import Socrata
 import psycopg2
@@ -9,9 +9,11 @@ from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def load_env_variables():
+# Función para cargar variables de entorno
+def load_env_variables(**kwargs):
     load_dotenv('/opt/airflow/dags/.env')
-    return {
+    global env_variables
+    env_variables = {
         'uri': os.getenv('URI'),
         'token': os.getenv('TOKEN'),
         'redshift_username': os.getenv('REDSHIFT_USERNAME'),
@@ -29,53 +31,53 @@ def load_env_variables():
         'error_db': os.getenv('ERRORDB', 'false').lower() == 'true'  
     }
 
-def send_alert_email(subject, body, from_email, to_email):
-    message = Mail(
-        from_email=from_email,
-        to_emails=to_email,
-        subject=subject,
-        html_content=f"""
-            <html>
-                <body style="font-family: Arial, sans-serif; margin: 20px;">
-                    <h2 style="color: #333;">{subject}</h2>
-                    <p style="font-size: 14px; color: #555;">{body}</p>
-                    <p style="font-size: 12px; color: #999;">Este mensaje fue generado automáticamente. Por favor, no responder.</p>
-                </body>
-            </html>
-        """
-    )
-    try:
-        sg = SendGridAPIClient(env_variables['sendgrid_api_key'])
-        response = sg.send(message)
-        print(f"Email enviado, Status Code: {response.status_code}")
-    except Exception as e:
-        print(f"Error enviando email: {str(e)}")
-
-def fetch_data_from_socrata(uri, token, min_api, error_api, from_email, to_email):
+def fetch_data_from_socrata(**kwargs):
+    uri = env_variables['uri']
+    token = env_variables['token']
+    min_api = env_variables['min_api']
+    error_api = env_variables['error_api']
+    from_email = env_variables['from_email']
+    to_email = env_variables['to_email']
+    
     try:
         client = Socrata(uri, token)
-        results = 0
+        results = []
         offset = 0
         df = pd.DataFrame()
+        
+        # Obtener las fechas de inicio y fin de los últimos 30 días
+        fecha_fin = datetime.now()
+        fecha_inicio = fecha_fin - timedelta(days=30)
 
-        while results != []:
-            results = client.get("v8jr-kywh", limit=50000, offset=offset)
+        # Formato de las fechas para la consulta
+        fecha_inicio_str = fecha_inicio.strftime("%Y-%m-%dT%H:%M:%S")
+        fecha_fin_str = fecha_fin.strftime("%Y-%m-%dT%H:%M:%S")    
+
+        while True:
+            results = client.get("v8jr-kywh", limit=50000, offset=offset, where=f"fecha_venta between '{fecha_inicio_str}' and '{fecha_fin_str}'")
+            if not results:
+                break
             data = pd.DataFrame.from_records(results)
+            df = pd.concat([df, data], ignore_index=True)
             offset += 50000
 
-            if results != []:
-                df = pd.concat([df, data], axis=0)
-
         if df.shape[0] < min_api:
-            send_alert_email("Alerta: Datos descargados insuficientes", f"Los datos descargados desde la API ({df.shape[0]}) son menores al mínimo requerido ({min_api}).", from_email, to_email)
-        return df
-
+            kwargs['ti'].xcom_push(key='send_email', value=True)
+            kwargs['ti'].xcom_push(key='email_subject', value="Alerta: Datos descargados insuficientes")
+            kwargs['ti'].xcom_push(key='email_body', value=f"Los datos descargados desde la API ({df.shape[0]}) son menores al mínimo requerido ({min_api}).")
+        
+        kwargs['ti'].xcom_push(key='fetched_data', value=df.to_json())
+    
     except Exception as e:
         if error_api:
-            send_alert_email("Error descargando datos", f"Error al descargar datos de la API: {str(e)}", from_email, to_email)
+            kwargs['ti'].xcom_push(key='send_email', value=True)
+            kwargs['ti'].xcom_push(key='email_subject', value="Error descargando datos")
+            kwargs['ti'].xcom_push(key='email_body', value=f"Error al descargar datos de la API: {str(e)}")
         raise
 
-def transform_data(df):
+def transform_data(**kwargs):
+    df = pd.read_json(kwargs['ti'].xcom_pull(key='fetched_data'))
+    # Transformación de datos
     df['fecha_venta'] = pd.to_datetime(df['fecha_venta'])
     df['anio_venta'] = df['anio_venta'].astype('int64')
     df['mes_venta'] = df['mes_venta'].astype('int64')
@@ -87,7 +89,6 @@ def transform_data(df):
     df['vehiculos_atendidos'] = df['vehiculos_atendidos'].astype('int64')
     df['cantidad_volumen_suministrado'] = df['cantidad_volumen_suministrado'].astype('float')
     df['date_update'] = pd.Timestamp(datetime.now())
-
     df['id_venta'] = (
         df['fecha_venta'].astype(str) + '_' +
         df['anio_venta'].astype(str) + '_' +
@@ -97,10 +98,15 @@ def transform_data(df):
         df['longitud'].astype(str)
     ).apply(hash)
 
-    return df
+    kwargs['ti'].xcom_push(key='transformed_data', value=df.to_json())
 
-def load_data_to_redshift(df, keys):
+def load_data_to_redshift(**kwargs):
+    # Recuperar el DataFrame transformado de XCom
+    df = pd.read_json(kwargs['ti'].xcom_pull(key='transformed_data'))
+    keys = env_variables  # Obtener las variables de entorno cargadas
+    
     try:
+        # Conexión a la base de datos Redshift
         conn = psycopg2.connect(
             dbname=keys['redshift_database'],
             user=keys['redshift_username'],
@@ -118,9 +124,9 @@ def load_data_to_redshift(df, keys):
                 WHERE table_schema = '{keys['redshift_schema']}'
                 AND table_name = 'daily_sales_data';
             """)
-
             table_exists = cursor.fetchone()[0] == 1
 
+            # Crear la tabla si no existe
             if not table_exists:
                 cursor.execute(f"""
                     CREATE TABLE {keys['redshift_schema']}.daily_sales_data (
@@ -139,12 +145,14 @@ def load_data_to_redshift(df, keys):
                     );
                 """)
 
+            # Preparar las consultas de actualización e inserción
             update_query = f"""
                 UPDATE {keys['redshift_schema']}.daily_sales_data
                 SET eds_activas = %s,
                     numero_de_ventas = %s,
                     vehiculos_atendidos = %s,
-                    cantidad_volumen_suministrado = %s
+                    cantidad_volumen_suministrado = %s,
+                    date_update = %s
                 WHERE fecha_venta = %s
                 AND anio_venta = %s
                 AND mes_venta = %s
@@ -177,74 +185,123 @@ def load_data_to_redshift(df, keys):
                     row['cantidad_volumen_suministrado'],
                     row['date_update']
                 )
-                for index, row in df.iterrows()
+                for _, row in df.iterrows()
             ]
 
-            from psycopg2.extras import execute_values
-
-            # Ejecutar actualización
+            # Ejecutar actualización antes de insertar
             for value in values:
                 cursor.execute(update_query, (
                     value[6],  # eds_activas
                     value[7],  # numero_de_ventas
                     value[8],  # vehiculos_atendidos
                     value[9],  # cantidad_volumen_suministrado
+                    value[10], # date_update
                     value[1],  # fecha_venta
                     value[2],  # anio_venta
                     value[3],  # mes_venta
                     value[4],  # dia_venta
                     value[5],  # latitud
-                    value[5]   # longitud
+                    value[6]   # longitud
                 ))
 
-            # Ejecutar inserción
+                        # Insertar nuevos datos
+            from psycopg2.extras import execute_values
             execute_values(cursor, insert_query, values)
             print("Datos cargados y actualizados exitosamente en Redshift.")
-
-            # Alerta si los valores insertados son cero
-            if len(values) == 0:
-                send_alert_email(
-                    "Alerta: No se insertaron datos",
-                    "No se insertaron datos en la tabla de Redshift.",
-                    keys['from_email'],
-                    keys['to_email']
-                )
-
+    
     except psycopg2.Error as e:
-        print(f"Error al conectar o cargar datos en Redshift: {e}")
-        # Enviar alerta si hay un error de conexión
-        send_alert_email(
-            "Error al cargar datos en Redshift",
-            f"Error al conectar o cargar datos en Redshift: {e}",
-            keys['from_email'],
-            keys['to_email']
-        )
+        # Manejar errores y enviar un correo de alerta si hay problemas
+        kwargs['ti'].xcom_push(key='send_email', value=True)
+        kwargs['ti'].xcom_push(key='email_subject', value="Error al cargar datos en Redshift")
+        kwargs['ti'].xcom_push(key='email_body', value=f"Error al conectar o cargar datos en Redshift: {e}")
+        raise
     finally:
         if conn:
             conn.close()
 
-def ingest_data_to_redshift():
-    keys = load_env_variables()
-    df = fetch_data_from_socrata(keys['uri'], keys['token'], keys['min_api'], keys['error_api'], keys['from_email'], keys['to_email'])
-    df = transform_data(df)
-    load_data_to_redshift(df, keys)
+def send_alert_email(**kwargs):
+    send_email = kwargs['ti'].xcom_pull(key='send_email', default_var=False)
+    if send_email:
+        subject = kwargs['ti'].xcom_pull(key='email_subject')
+        body = kwargs['ti'].xcom_pull(key='email_body')
+        message = Mail(
+            from_email=env_variables['from_email'],
+            to_emails=env_variables['to_email'],
+            subject=subject,
+            html_content=f"""
+                <html>
+                    <body style="font-family: Arial, sans-serif; margin: 20px;">
+                        <h2 style="color: #333;">{subject}</h2>
+                        <p style="font-size: 14px; color: #555;">{body}</p>
+                        <p style="font-size: 12px; color: #999;">Este mensaje fue generado automáticamente. Por favor, no responder.</p>
+                    </body>
+                </html>
+            """
+        )
+        try:
+            sg = SendGridAPIClient(env_variables['sendgrid_api_key'])
+            sg.send(message)
+            print("Correo de alerta enviado con éxito.")
+        except Exception as e:
+            print(f"Error enviando correo de alerta: {str(e)}")
 
 # Configuración del DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
 dag = DAG(
     dag_id='daily_sales_data_ingestion',
-    default_args={
-        'owner': 'airflow',
-        'start_date': datetime(2024, 1, 1),
-        'retries': 1,
-    },
+    default_args=default_args,
     schedule_interval='@daily',
-    catchup=False,
+    catchup=True,  # Permitir mecanismo de backfill
     description='DAG for daily ingestion of sales data into Redshift'
 )
 
-# Operador de Python para ejecutar la función
-ingest_data_task = PythonOperator(
-    task_id='ingest_data_to_redshift',
-    python_callable=ingest_data_to_redshift,
+# Definición de tareas
+
+# Cargar variables de entorno
+load_env_task = PythonOperator(
+    task_id='load_env_variables',
+    python_callable=load_env_variables,
+    provide_context=True,
     dag=dag
 )
+
+# Descargar datos desde Socrata
+fetch_data_task = PythonOperator(
+    task_id='fetch_data_from_socrata',
+    python_callable=fetch_data_from_socrata,
+    provide_context=True,
+    dag=dag
+)
+
+# Transformar datos
+transform_data_task = PythonOperator(
+    task_id='transform_data',
+    python_callable=transform_data,
+    provide_context=True,
+    dag=dag
+)
+
+# Cargar datos a Redshift
+load_data_task = PythonOperator(
+    task_id='load_data_to_redshift',
+    python_callable=load_data_to_redshift,
+    provide_context=True,
+    dag=dag
+)
+
+# Enviar correo de alerta si es necesario
+send_email_task = PythonOperator(
+    task_id='send_alert_email',
+    python_callable=send_alert_email,
+    provide_context=True,
+    dag=dag
+)
+
+# Definición de dependencias entre tareas
+load_env_task >> fetch_data_task >> transform_data_task >> load_data_task >> send_email_task
